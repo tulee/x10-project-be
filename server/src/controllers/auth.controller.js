ad257547e153a57ff691b2dbb15832a1fa7c6e46
tulee <lyanhtu187@gmail.com>
2023-05-09 12:43:51 +0700

    fix

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -11,116 +11,127 @@ class AuthController {
     constructor() {}
 
     register = async (req,res) => {
-      const errors = validationResult(req);
-      if(!errors.isEmpty()){
-        res.status(400).json({ status:"false", data: errors.array(), message:"Lỗi khi đăng kí" });
-        return
-      }
-
-      const username = req.body.username.toLowerCase()
-	    const cbnv = await cbnvModel.getCbnvByUsername(username)
-
-      if(cbnv){
-        res.status(400).json({status:"false",data:[{
-            type: "field",
-            value: username,
-            msg: "Tên tài khoản đã tồn tại",
-            path: "username",
-            location: "body"
-          }], message:"Lỗi khi đăng kí"})
-        return
-      } else {
-        const hashPassword = bcrypt.hashSync(req.body.password, SALT_ROUNDS);
-        const newCbnv = {
-          username: username,
-          password: hashPassword,
+      try {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+          res.status(400).json({ status:"false", data: errors.array(), message:"Lỗi khi đăng kí" });
+          return
         }
 
-        const createdCbnv = await cbnvModel.create(newCbnv)
+        const username = req.body.username.toLowerCase()
+        const cbnv = await cbnvModel.getCbnvByUsername(username)
 
-        if(!createdCbnv){
-          res.status(408).json({status:"false", message:"Có lỗi trong quá trình tạo tài khoản, vui lòng thử lại"})
+        if(cbnv){
+          res.status(400).json({status:"false",data:[{
+              type: "field",
+              value: username,
+              msg: "Tên tài khoản đã tồn tại",
+              path: "username",
+              location: "body"
+            }], message:"Lỗi khi đăng kí"})
           return
         } else {
-          res.status(200).json({status:"true",data:username, message:"Tạo tài khoản thành công"})
-          return
-        }
+          const hashPassword = bcrypt.hashSync(req.body.password, SALT_ROUNDS);
+          const newCbnv = {
+            username: username,
+            password: hashPassword,
+          }
 
+          const createdCbnv = await cbnvModel.create(newCbnv)
+
+          if(!createdCbnv){
+            res.status(408).json({status:"false", message:"Có lỗi trong quá trình tạo tài khoản, vui lòng thử lại"})
+            return
+          } else {
+            res.status(200).json({status:"true",data:username, message:"Tạo tài khoản thành công"})
+            return
+          }
+
+        }
+      } catch (error) {
+        console.log(error);
+        res.status(408).json({status:"false", data:error, message:"Lỗi khi tạo tài khoản, xin vui lòng thử lại"})
       }
     }
 
     login = async (req,res) => {
-      const errors = validationResult(req);
-      if(!errors.isEmpty()){
-        res.status(400).json({ status:"false", data: errors.array(), message:"Lỗi khi đăng nhập" });
-        return
-      }
-
-      const username = req.body.username.toLowerCase() || 'test';
-	    const password = req.body.password || '12345';
-
-      const cbnv = await cbnvModel.getCbnvByUsername(username)
-      if(!cbnv){
-        res.status(404).json({status:"false",data:[{
-          type: "field",
-          value: username,
-          msg: "Tên đăng nhập không tồn tại",
-          path: "username",
-          location: "body"
-        }], message:"Lỗi khi đăng nhập"})
-
-        return
-      }
-
-      const isPasswordValid = bcrypt.compareSync(password, cbnv.password);
-      if (!isPasswordValid) {
-        res.status(404).json({status:"false",data:[{
-          type: "field",
-          value: password,
-          msg: "Mật khẩu không chính xác",
-          path: "password",
-          location: "body"
-        }], message:"Lỗi khi đăng nhập"})
-        
-        return
-      }
-
-      const accessTokenLife =
-      process.env.ACCESS_TOKEN_LIFE || jwtVariable.accessTokenLife;
-      const accessTokenSecret =
-        process.env.ACCESS_TOKEN_SECRET || jwtVariable.accessTokenSecret;
-
-      const dataForAccessToken = {
-        username: cbnv.username,
-      };
-
-      const accessToken = await authService.generateToken(
-        dataForAccessToken,
-        accessTokenSecret,
-        accessTokenLife,
-      )
-
-      if (!accessToken) {
-        return res.send({status:"false", message:"Đăng nhập không thành công, vui lòng thử lại"});
-      }
-
-	    let refreshToken = randToken.generate(jwtVariable.refreshTokenSize)
-
-      if (!cbnv.refreshToken) {
-        await cbnvModel.updateCbnvByUsername(cbnv.username, {refreshToken: refreshToken});
-      } else {
-        refreshToken = cbnv.refreshToken;
-      }
-
-      res.send({
-        status:"true",
-        message:"Đăng nhập thành công",
-        data:{
-          accessToken,
-          refreshToken,
-          cbnv,
+      try {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+          res.status(400).json({ status:"false", data: errors.array(), message:"Lỗi khi đăng nhập" });
+          return
         }
-      })
+
+        const username = req.body.username.toLowerCase() || 'test';
+        const password = req.body.password || '12345';
+
+        const cbnv = await cbnvModel.getCbnvByUsername(username)
+        if(!cbnv){
+          res.status(404).json({status:"false",data:[{
+            type: "field",
+            value: username,
+            msg: "Tên đăng nhập không tồn tại",
+            path: "username",
+            location: "body"
+          }], message:"Lỗi khi đăng nhập"})
+
+          return
+        }
+
+        const isPasswordValid = bcrypt.compareSync(password, cbnv.password);
+        if (!isPasswordValid) {
+          res.status(404).json({status:"false",data:[{
+            type: "field",
+            value: password,
+            msg: "Mật khẩu không chính xác",
+            path: "password",
+            location: "body"
+          }], message:"Lỗi khi đăng nhập"})
+          
+          return
+        }
+
+        const accessTokenLife =
+        process.env.ACCESS_TOKEN_LIFE || jwtVariable.accessTokenLife;
+        const accessTokenSecret =
+          process.env.ACCESS_TOKEN_SECRET || jwtVariable.accessTokenSecret;
+
+        const dataForAccessToken = {
+          username: cbnv.username,
+        };
+
+        const accessToken = await authService.generateToken(
+          dataForAccessToken,
+          accessTokenSecret,
+          accessTokenLife,
+        )
+
+        if (!accessToken) {
+          res.status(408).json({status:"false", message:"Đăng nhập không thành công, vui lòng thử lại"});
+          return
+        }
+
+        let refreshToken = randToken.generate(jwtVariable.refreshTokenSize)
+
+        if (!cbnv.refreshToken) {
+          await cbnvModel.updateCbnvByUsername(cbnv.username, {refreshToken: refreshToken});
+        } else {
+          refreshToken = cbnv.refreshToken;
+        }
+
+        res.send({
+          status:"true",
+          message:"Đăng nhập thành công",
+          data:{
+            accessToken,
+            refreshToken,
+            cbnv,
+          }
+        })
+      } catch (error) {
+        console.log(error);
+        res.status(408).json({status:"false", data:error, message:"Lỗi khi đăng nhập, xin vui lòng thử lại"})
+      }
     }
 
     refreshToken = async (req,res) => {
